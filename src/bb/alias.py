fa4ef1244a58cb5e322a80627debd7068b18b8d0
tjk <tkells@atlassian.com>
2024-06-04 11:27:44 -0500

    Added basic alias command

---
+++ src/bb/alias.py
@@ -0,0 +1,27 @@
+import click
+
+from bb.config import BBConfig
+
+
+@click.group()
+def alias():
+    pass
+
+@alias.command()
+@click.argument("alias_name")
+@click.argument("cmd")
+def set(alias_name, cmd):
+    conf = BBConfig()
+    conf.update(f"alias.{alias_name}", cmd)
+    conf.write()
+
+    print(f"[bold]Successfully added alias {alias_name} for {cmd}")
+
+
+@alias.command()
+@click.argument("alias_name")
+def remove(alias_name):
+    conf = BBConfig()
+    conf.delete(f"alias.{alias_name}")
+    conf.write()
+    print(f"[bold]Successfully removed alias {alias_name}")
