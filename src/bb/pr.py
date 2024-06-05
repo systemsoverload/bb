afd0a9990e8e087d8a0ad0738af5d2e1a97947cd
tjk <tkells@atlassian.com>
2024-06-04 23:52:52 -0500

    Add `pr create` command

--- src/bb/pr.py
+++ src/bb/pr.py
@@ -1,8 +1,17 @@
 import click
+from rich import print
 from rich.console import Console
+from rich.prompt import Prompt
 from rich.table import Table
 
-from bb.api import WEB_BASE_URL, get_prs
+from bb.api import WEB_BASE_URL, create_pr, get_prs
+from bb.git import (
+    IPWhitelistException,
+    get_branch,
+    get_current_branch,
+    get_default_branch,
+    push_branch,
+)
 from bb.utils import repo_context_command
 
 
@@ -37,7 +46,12 @@ def pr():
 @repo_context_command
 def list(repo_slug, _all, mine, reviewing):
     """Fetch your open pullrequests from current repository"""
-    prs = get_prs(repo_slug, _all, reviewing, mine)
+    with Console().status("Fetching pull requests..."):
+        try:
+            prs = get_prs(repo_slug, _all, reviewing, mine).unwrap()
+        except IPWhitelistException as e:
+            print(f"{e}")
+            return
 
     # Default to --mine, cant quite figure out how to do this in click.option directly
     if not any([_all, reviewing, mine]):
@@ -50,7 +64,7 @@ def list(repo_slug, _all, mine, reviewing):
     elif mine:
         title = "My open pullrequests"
     else:
-        title =f"PR {_all}{reviewing}{mine}"
+        title = f"PR {_all}{reviewing}{mine}"
 
     table = Table(title=title)
     table.add_column("Id", justify="left", style="cyan")
@@ -71,3 +85,45 @@ def list(repo_slug, _all, mine, reviewing):
         )
     console = Console()
     console.print(table)
+
+
+@pr.command()
+@click.option("--title", "-t", help="PR title")
+@click.option("--description", "-d", help="PR description")
+@click.option("--src", "-s", help="Source branch for pull request (default [current branch])")
+@click.option("--dest", "-d", help="Destination branch for pull request (default [current branch])")
+@click.option("--close-source-branch", "-c", is_flag=True, default=True, help="Close source branch after merge [bool]")
+@repo_context_command
+def create(repo_slug, title, description, close_source_branch, src, dest):
+    # TODO - Check if the PR exists on BB already
+
+    if not src:
+        src = get_current_branch().unwrap()
+    else:
+        try:
+            src = get_branch(src).unwrap()
+        except Exception:
+            print(f"[bold red]Unable to find branch {src}")
+            return
+
+    dest = dest or get_default_branch().unwrap()
+
+    # If branch not pushed, do it now
+    try:
+        push_branch(src).unwrap()
+    except IPWhitelistException as e:
+        print(f"[bold red]{e}")
+        return
+
+    # TODO - Fetch description template and do something cool with it?
+
+    print(f"Create new pull request for [bold blue]{src}[/] into [bold blue]{dest}[/] for {repo_slug}")
+
+    if not title:
+        title = Prompt.ask("[bold]Title")
+    if not description:
+        description = Prompt.ask("[bold]Description (enter to default to generated description)")
+
+    with Console().status("Creating pull request"):
+        res = create_pr(repo_slug, title, src, dest, description, close_source_branch).unwrap()
+    print(f"Successfully created PR - {res.json()['links']['html']['href']}")
