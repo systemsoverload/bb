2ba51042c8c645549dfd7dde54947ce06d8ac5c7
tjk <tkells@atlassian.com>
2024-12-12 20:53:53 -0600

    refactor: drop dead models

--- src/bb/models/__init__.py
+++ src/bb/models/__init__.py
@@ -1,47 +1,14 @@
-from dataclasses import dataclass
-from enum import Enum, auto
-from typing import Dict, Optional
-
 from .base import BaseModel, BitbucketClient
 from .filediff import FileDiff
 from .pullrequest import PullRequest
 from .repository import PullRequestCollection, Repository
 
 
-class ViewState(Enum):
-    """Available view states in the TUI"""
-
-    LIST = auto()
-    DETAIL = auto()
-    DIFF = auto()
-    SEARCH = auto()
-
-
-@dataclass
-class User:
-    """Bitbucket user information"""
-
-    display_name: str
-    uuid: str
-    account_id: Optional[str] = None
-    links: Optional[Dict] = None
-
-
-@dataclass
-class Branch:
-    """Repository branch information"""
-
-    name: str
-    target: Dict
-
-
 __all__ = [
     "BaseModel",
-    "Branch",
     "BitbucketClient",
     "FileDiff",
     "PullRequest",
     "PullRequestCollection",
     "Repository",
-    "User",
 ]
